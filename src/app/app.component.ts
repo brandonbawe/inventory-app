802b80ea6b2844f97a0d1dec7eb9fa8235c8c02c
brandonbawe <dbrandonbawe@gmail.com>
2020-09-26 23:31:26 +0100

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Product } from './product.model';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'inventory-app';
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
+    ];
+  }
+  function productWasSelected(product: Product): void {
+    console.log("Product clicked: ", )
+}
+}
